da2b7feb1e3b427de0f0d0fb67c2cbb7ef61a714
Gavin Moore <gavizoid16@gmail.com>
2018-08-20 02:35:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function prependKitten(name) {
   return ['Arnold', ...kittens]
 }
 function removeLastKitten() {
-  kittens.slice(0, kittens.length )
+  kittens.slice(0, kittens.length - 1)
   return kittens
 }
